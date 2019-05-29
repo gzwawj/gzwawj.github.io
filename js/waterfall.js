355657db2c2c71d1ed1cab78474f250f76a86504
gzwawj <936588357@qq.com>
2019-05-29 13:09:30 +0800

    添加演示

--- js/waterfall.js
+++ js/waterfall.js
@@ -14,14 +14,154 @@
      */
   var demoContent = [
     {
-      demo_link: '',
-      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/calculate/img/demoing.jpg',
-      github_link: 'https://github.com/gzwawj/node-demo',
-      codepen_link: '',
+      demo_link: 'https://output.jsbin.com/kinijon/',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/calculate/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/calculate',
+      codepen_link: 'https://codepen.io/gzwawj/pen/byKpWK',
       jsfiddle_link: '',
-      title: 'node搭建的小网站demo',
-      core_tech: 'nodejs',
-      description: '使用node开发一个增删改的用户管理系统'
+      title: 'calculate比例',
+      core_tech: 'html,css,js',
+      description: '给出宽度或者高度，快速计算比例结果'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/yuyufaz',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/9x9Table/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/9x9Table',
+      codepen_link: 'https://codepen.io/gzwawj/pen/byKwYq',
+      jsfiddle_link: '',
+      title: '九九乘法表',
+      core_tech: 'html,css,js',
+      description: '九九乘法表'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/nojoquf',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/ascii/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/ascii',
+      codepen_link: 'https://codepen.io/gzwawj/pen/RmJNgP',
+      jsfiddle_link: '',
+      title: 'ascii转换',
+      core_tech: 'html,css,js',
+      description: '输入字符转换为ascii，二进制，八进制，十进制，十六进制'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/giqodir/',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/canvasClock/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/canvasClock',
+      codepen_link: 'https://codepen.io/gzwawj/pen/xNzOKq',
+      jsfiddle_link: '',
+      title: 'canvas时钟',
+      core_tech: 'html,css,js',
+      description: '使用canvas创建的心形动画时钟'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/huhusor/',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/canvasDisc/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/canvasDisc',
+      codepen_link: 'https://codepen.io/gzwawj/pen/JqZXzE',
+      jsfiddle_link: '',
+      title: 'Canvas圆盘抽奖',
+      core_tech: 'html,css,js',
+      description: 'Canvas圆盘抽奖'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/sacunit',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/createQrcode/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/createQrcode',
+      codepen_link: 'https://codepen.io/gzwawj/pen/JqZbBR',
+      jsfiddle_link: '',
+      title: 'js生成二维码',
+      core_tech: 'html,css,js',
+      description: 'js生成二维码,可以自定义输入内容'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/galusin/',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/css3-3D/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/css3-3D',
+      codepen_link: 'https://codepen.io/gzwawj/pen/PvazZX',
+      jsfiddle_link: '',
+      title: 'css3原生3D',
+      core_tech: 'html,css,js',
+      description: '使用css3原生代码创建3D效果'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/nucupiw',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/fanMenu/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/fanMenu',
+      codepen_link: 'https://codepen.io/gzwawj/pen/KLeNVx',
+      jsfiddle_link: '',
+      title: '纯css扇形菜单',
+      core_tech: 'html,css,js',
+      description: '纯css扇形菜单'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/vayenab',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/fileCreateAndDown/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/fileCreateAndDown',
+      codepen_link: 'https://codepen.io/gzwawj/pen/pmKNoO',
+      jsfiddle_link: '',
+      title: '创建文件与下载',
+      core_tech: 'html,css,js',
+      description: '通过js创建文件与下载'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/muyotub',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/jsPage/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/jsPage',
+      codepen_link: 'https://codepen.io/gzwawj/pen/qGKNgy',
+      jsfiddle_link: '',
+      title: 'js实现分页',
+      core_tech: 'html,css,js',
+      description: 'js实现分页'
+    },
+    {
+      demo_link: 'https://codepen.io/gzwawj/pen/BeVpav',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/rightMenu/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/rightMenu',
+      codepen_link: 'https://codepen.io/gzwawj/pen/BeVpav',
+      jsfiddle_link: '',
+      title: 'js右键菜单',
+      core_tech: 'html,css,js',
+      description: 'js右键菜单'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/jeyigen',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/roller/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/roller',
+      codepen_link: 'https://codepen.io/gzwawj/pen/eaKBXN',
+      jsfiddle_link: '',
+      title: '鼠标滚轮控制图片大小',
+      core_tech: 'html,css,js',
+      description: '鼠标滚轮控制图片大小'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/vijumir',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/scroll/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/scroll',
+      codepen_link: 'https://codepen.io/gzwawj/pen/BeVQwx',
+      jsfiddle_link: '',
+      title: '滚动条在底部',
+      core_tech: 'html,css,js',
+      description: '随着内容的增多，滚动条会一直在在底部'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/hujuciv',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/sinAnimation/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/sinAnimation',
+      codepen_link: 'https://codepen.io/gzwawj/pen/KLegav',
+      jsfiddle_link: '',
+      title: 'sin绘制动画',
+      core_tech: 'html,css,js',
+      description: '通过sin函数绘制动画效果，实现变化快慢的不同'
+    },
+    {
+      demo_link: 'https://output.jsbin.com/wivexom',
+      img_link: 'https://raw.githubusercontent.com/gzwawj/html-css-js/master/snake/img/demoimg.jpg',
+      github_link: 'https://github.com/gzwawj/html-css-js/tree/master/snake',
+      codepen_link: 'https://codepen.io/gzwawj/pen/YbvpZN',
+      jsfiddle_link: '',
+      title: '游戏贪吃蛇',
+      core_tech: 'html,css,js',
+      description: 'js代码实现的游戏贪吃蛇'
     }
   ];
 
