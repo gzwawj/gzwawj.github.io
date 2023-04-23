77bc18c74186e3a712745e65eb365fee8af88ffd
gzwawj <936588357@qq.com>
2023-04-23 08:37:46 +0800

    sw

--- sw.js
+++ sw.js
@@ -1,28 +1,32 @@
 self.assets = [
 	'/favicon.ico',
 	'/css/font-awesome.min.css',
-	'/fonts/fontawesome-webfont.eot',
-	'/fonts/fontawesome-webfont.svg',
-	'/fonts/fontawesome-webfont.ttf',
-	'/fonts/fontawesome-webfont.woff',
-	'/fonts/fontawesome-webfont.woff2',
-	'/fonts/FontAwesome.otf',
+	'/fonts/fontawesome-webfont.eot?v=4.7.0',
+	'/fonts/fontawesome-webfont.svg?v=4.7.0',
+	'/fonts/fontawesome-webfont.ttf?v=4.7.0',
+	'/fonts/fontawesome-webfont.woff?v=4.7.0',
+	'/fonts/fontawesome-webfont.woff2?v=4.7.0',
 	'/css/style.css',
 	'/js/masonry.pkgd.min.js',
 	'/js/smooth-scroll.min.js',
 	'/js/main.min.js',
 ];
-self.assetsVersion = "9116f1ca39faad37cd26fe92bbef6f3d";
 //然后引入workbox
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.0/workbox-sw.js');
 workbox.setConfig({
 	debug: false,
 	modulePathPrefix: 'https://storage.googleapis.com/workbox-cdn/releases/3.3.0/'
 });
+workbox.core.setCacheNameDetails({
+	prefix: 'gzwawj',
+	suffix: 'v1',
+	precache: 'precache',
+	runtime: 'runtime'
+})
 //直接激活跳过等待阶段
 workbox.skipWaiting();
 workbox.clientsClaim();
-
+self.assetsVersion = workbox.core.cacheNames.runtime
 workbox.precaching.precacheAndRoute(self.assets);
 //静态资源采用staleWhileRevalidate策略，安全可靠
 workbox.routing.registerRoute(
@@ -43,7 +47,7 @@ workbox.routing.registerRoute(
 	})
 );
 workbox.routing.registerRoute(
-	new RegExp('www\.intensedebate\.com.*.(?:js|css|ico|png|gif)', ''),
+	new RegExp('https:\/\/www\.intensedebate\.com.*.(?:js|css|ico|png|gif)', ''),
 	workbox.strategies.staleWhileRevalidate({
 		cacheName: self.assetsVersion,
 		plugins: [
